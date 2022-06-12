9817fa995d277c48ddd13ea7d2411796dd90738f
nivyapeter <nivyakpeter@gmail.com>
2022-06-12 14:34:58 +0530

    feat: added search data block

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { VscSearch } from "react-icons/vsc";
 import { BsCloudSun } from "react-icons/bs";
 import { useState } from "react";
+import SearchData from "./SearchData";
 
 function SearchBar() {
   const [data, setData] = useState(false);
@@ -10,23 +11,22 @@ function SearchBar() {
   }
   return (
     <div class="flex">
-      <div class="xl:w-96 ">
-        <div class="flex md:pb-1">
+      <div class="xl:w-80 ">
+        <div class="flex md:pb-1 rounded-3xl bg-[#337A9E] dataBlock items-center">
+          <div>
           <input
             type="search"
-            className=" rounded-3xl placeholder-white font-medium relative flex-auto focus:outline-2 outline-white text-center min-w-0 block w-full py-[5px] px-3 md:py-1.5 text-base text-white  bg-[#337A9E]"
+            className=" rounded-3xl placeholder-white bg-[#337A9E]  focus:outline-none font-medium relative flex-auto text-center min-w-0 block w-full py-[5px] px-3 md:py-1.5 text-base text-white  "
             placeholder="Search City or Postcode"
+            onFocus={showData}
+            onBlur={()=>setData(false)}
           />
-          <div className="text-white absolute left-[70%] top-[5%] font-bold pr-2 ">
+          </div>
+            <div className="text-white font-bold pr-2 ">
             <VscSearch className="text-white" />
           </div>
         </div>
-        <div className="bg-white rounded-lg hidden focus:block absolute w-96 dataBlock">
-          <div className="border-b-2 p-3">
-            <span>Recents</span>
-          </div>
-          <div>You Have No Recent Locations</div>
-        </div>
+        {data === true? <SearchData /> :null}
       </div>
     </div>
   );
